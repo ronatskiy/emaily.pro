8e527f624f2a5a216ae2a7129f3c84e7d9f1393a
ronatskiy <r.onatskiy@gmail.com>
2017-09-20 17:18:00 +0300

    fix passport strategy config

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: "http://localhost:5000/auth/google/callback",
+			callbackURL: "/auth/google/callback",
 		},
 		(accessToken, refreshToken, { id }, done) => {
 			User.findOne({ googleId: id }).then(existingUser => {
