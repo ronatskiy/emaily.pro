dc19a71cb02b72a152e29002bd713cc00998c6a0
ronatskiy <r.onatskiy@gmail.com>
2017-09-20 18:30:20 +0300

    try to fix mongoose connection

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
-mongoose.connect(keys.mongoURI, {
-	useMongoClient: true,
-});
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(
